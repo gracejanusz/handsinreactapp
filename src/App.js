69477793176d4b82fc1c5eb01d52f6d2432bef68
grace <71283980+gracejanusz@users.noreply.github.com>
2025-05-21 19:43:11 -0500

    Revamped template

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './HandShortLogo.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className='custom-font'>
+          A new way to learn.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
